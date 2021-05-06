f6b8e46b7423a6c42f4470a827c51eaaa9454d36
Nguyen Minh Canh <canhnm@cps.onl>
2021-05-06 17:30:46 +0700

    test

--- resources/js/components/IndexField.vue
+++ resources/js/components/IndexField.vue
@@ -1,11 +1,11 @@
 <template>
     <span class="flex flex-row overflow-x-auto scrolling-touch" style="width: 10rem;">
-        <img
+        <!-- <img
             v-for="img in JSON.parse(field.value)"
             :src="img.url"
             :alt="img.name"
             class="rounded h-8 mr-1"
-        >
+        > -->
     </span>
 </template>
 
