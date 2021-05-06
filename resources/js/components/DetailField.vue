63fdf28f3b8c007260b39fba6c325fd756f9b4f7
Nguyen Minh Canh <canhnm@cps.onl>
2021-05-06 17:35:46 +0700

    test

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -1,13 +1,13 @@
 <template>
     <panel-item :field="field">
         <div slot="value" class="flex flex-row overflow-x-auto py-2">
-            <img
+            <!-- <img
                 v-for="img in JSON.parse(field.value)"
                 :src="img.url"
                 :alt="img.name"
                 style="height: 8rem; width: auto;"
                 class="rounded shadow-md mr-2"
-            >
+            > -->
         </div>
     </panel-item>
 </template>
